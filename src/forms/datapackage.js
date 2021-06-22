b5cb417c7e15b6021bedb21a69f0a356075a1934
timeonator <timeonator@gmail.com>
2021-06-22 11:20:39 -0700

    basic form working and posting data

--- src/forms/datapackage.js
+++ src/forms/datapackage.js
@@ -26,17 +26,29 @@ var DataPackage = () => {
         const [license, setLicense]=useState("")
         const [sources, setSources]=useState("")
         const [resources, setResources]=useState("")
-        const [id, setId]=useState("")       
+        const [id, setId]=useState("") 
+        
+        
+        async function postData(url = '', data = {}) {
+            // Default options are marked with *
+            const response = await fetch(url, {
+              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors', // no-cors, *cors, same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              credentials: 'same-origin', // include, *same-origin, omit
+              headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              redirect: 'follow', // manual, *follow, error
+              referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+              body: JSON.stringify(data) // body data type must match "Content-Type" header
+            })
+
+            return response.json(); // parses JSON response into native JavaScript objects
+        }
 
 
-    const myData = {
-        "name": name,
-        "title": title,
-        "license": license,
-        "sources": [ sources ],
-        "resources": [ resources ],
-        "id": id,
-    }
     const handleClick = (e) => {
         const myData = {
             "name": name,
@@ -47,6 +59,7 @@ var DataPackage = () => {
             "id": id,
         }
         console.log(myData)
+        postData('http://localhost:8000/datapackage', myData)
     }
      return(
         <Container>
