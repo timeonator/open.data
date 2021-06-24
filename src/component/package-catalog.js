c4b5ab363b844506214126817ee4933cd43f6267
timeonator <timeonator@gmail.com>
2021-06-23 18:26:12 -0700

    Initial code for package catalog table

--- src/component/package-catalog.js
+++ src/component/package-catalog.js
@@ -1,6 +1,68 @@
+import { useTable } from "react-table";
+import '../App.css';
+
 const PackageCatalog = () => {
+    const columns = [
+        {
+          Header: "ID",
+          accessor: "_id",
+        },
+        {
+          Header: "Name",
+          accessor: "name",
+        },
+        {
+          Header: "Title",
+          accessor: "title",
+        },
+      ];
+    const data = [
+        {
+        _id:"60d226e8b5efc85ef8286a39",
+        name:"absolute",
+        title:"The Absolute Truth",
+        licenses:"MIT",
+        sources:"Rpbert Gifford",
+        resources:"absolute.csv",
+        },
+    ]
+
+    
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({
+        columns,
+        data,
+      });
     return(
-        <h3>Package Catalog</h3>
+
+        <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     )
 }
 export default PackageCatalog
