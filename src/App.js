6614280a1088a7e36d48c253a6333ec344e85801
timeonator <timeonator@gmail.com>
2021-07-09 00:30:42 -0700

    restructured for webpack

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import Navigation from './navigation'
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 import PackageCatalog from './component/package-catalog'
 import DatasetCatalog from './component/dataset-catalog'
 import DataPackage from './forms/datapackage'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        <Navigation />
-          <Switch>                
-             <Route path='/datapackage' component={DataPackage}/>
-             <Route path='/dataset-catalog' component={DatasetCatalog}/>
-             <Route path='/package-catalog' component={PackageCatalog}/>  
-             <Route exact path='/' component={Home}/>            
-          </Switch>
-    </div>
+    <Router>
+        <div className="App">
+            <Navigation />
+            <Switch>                
+                <Route path='/datapackage' component={DataPackage}/>
+                <Route path='/dataset-catalog' component={DatasetCatalog}/>
+                <Route path='/package-catalog' component={PackageCatalog}/>  
+                <Route exact path='/' component={Home}/>            
+            </Switch>
+        </div>
+    </Router>
+
   );
 }
 
