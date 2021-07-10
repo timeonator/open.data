8801f49f2cef975ebcbf785598405df889ccef72
timeonator <timeonator@gmail.com>
2021-07-09 20:15:22 -0700

    updated packages, new webpack.dev.config.js

---
+++ webpack.dev.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules : [
+            {
+                test: /\.js$/,
+                exclude: (/node_modules/),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    }
+                }                
+            }
+
+        ]
+    }
+}
