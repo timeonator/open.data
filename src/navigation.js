8d82bcb52762b2763fb9630a484acaa4820d0bcd
timeonator <timeonator@gmail.com>
2021-07-12 14:45:20 -0700

    added a fragment arount the JSX

--- src/navigation.js
+++ src/navigation.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { 
     Navbar, 
     Nav, 
@@ -9,27 +10,34 @@ import {
 
 const Navigation = () => {
     return(
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="/">OpenData</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/home">Home</Nav.Link>
-      <NavDropdown title="New" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/datapackage">Datapackage</NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="View" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/package-catalog">Package Catalog</NavDropdown.Item>
-        <NavDropdown.Item href="/dataset-catalog">Dataset Catalog</NavDropdown.Item>        
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-3" />
-      <Button variant="outline-success" size='sm'>Search</Button>
-    </Form>
-    
-  </Navbar.Collapse>
-</Navbar>
+    <>
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">OpenData</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link href="/datapackage">Datapackage</Nav.Link>
+                <Nav.Link href="/package-catalog">Package Catalog</Nav.Link>
+                <Nav.Link href="/dataset-catalog">Dataset Catalog</Nav.Link>        
+                {/* <NavDropdown title="New" id="basic-nav-dropdown">
+                    <NavDropdown.Item >
+                        <Nav.Link href="/datapackage">Datapackage</Nav.Link>
+                    </NavDropdown.Item>
+                </NavDropdown> */}
+               {/* <NavDropdown title="View" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/package-catalog">Package Catalog</NavDropdown.Item>
+                    <NavDropdown.Item href="/dataset-catalog">Dataset Catalog</NavDropdown.Item>        
+                </NavDropdown> */}
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-3" />
+                <Button variant="outline-success" size='sm'>Search</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    </>
+
     )
 
 }
