d7354ad6803ef2a38f3a55db287af7e7544119ab
timeonator <timeonator@gmail.com>
2021-07-13 23:58:41 -0700

    fetching problems

--- src/component/package-catalog.js
+++ src/component/package-catalog.js
@@ -1,7 +1,32 @@
+import React, {useState, useEffect } from 'react';
 import { useTable } from "react-table";
 import '../App.css';
 
+
 const PackageCatalog = () => {
+    const [data, setData]=useState([]);
+    const [loading, setLoading]=useState(true);
+    const [error, setError]=useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/datapackages",{
+            method: 'GET',
+            accept: 'application/json',
+            mode: 'cors',
+            cache: 'default',
+        })
+        .then(response => {
+            if (response.ok) { return response.json() }
+            throw response;
+        }).then(data => {
+            setData(data);
+        }).catch(error => {
+            setError(error);
+            console.log("Error fetching Data: ", error);
+        })
+            .finally(()=> {setLoading(false)});
+      },[]);
+
     const columns = [
         {
           Header: "ID",
@@ -16,17 +41,6 @@ const PackageCatalog = () => {
           accessor: "title",
         },
       ];
-    const data = [
-        {
-        _id:"60d226e8b5efc85ef8286a39",
-        name:"absolute",
-        title:"The Absolute Truth",
-        licenses:"MIT",
-        sources:"Rpbert Gifford",
-        resources:"absolute.csv",
-        },
-    ]
-
     
     const {
         getTableProps,
@@ -34,35 +48,45 @@ const PackageCatalog = () => {
         headerGroups,
         rows,
         prepareRow,
-      } = useTable({
+    } = useTable({
         columns,
         data,
-      });
+    });
+
+    if(loading) return(<h3>Loading Data ...</h3>);
+    if(error) return("<h3> Error</h3>");
+
     return(
+        <>
+        <h3>Package Catalog</h3>
 
         <table {...getTableProps()}>
         <thead>
-          {headerGroups.map((headerGroup) => (
+        {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
+            {headerGroup.headers.map((column) => (
                 <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
+            ))}
             </tr>
-          ))}
+        ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
+        {rows.map((row, i) => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()}>
+            <tr {...row.getRowProps()}>
                 {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
                 })}
-              </tr>
+            </tr>
             );
-          })}
+        })}
         </tbody>
-      </table>
-    )
+    </table>
+    </>
+    )  
+
+
+
 }
 export default PackageCatalog
