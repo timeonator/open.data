52f7c17acd05b87517c5ba9b6e3127ec4ac5160e
timeonator <timeonator@gmail.com>
2021-08-17 22:21:42 -0700

    added data-pacakge: component to display the table data of a package

--- src/component/package-catalog.js
+++ src/component/package-catalog.js
@@ -1,29 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import DataPackage from './data-package';
 //import ReactDataGrid from 'react-data-grid';
-import { useTable } from 'react-table';
 import './../App.css';
 
-const cloneDeep = require('lodash/clonedeep');
-
 const PackageCatalog = (props) => {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'ID',
-        accessor: '_id',
-      },
-      {
-        Header: 'Name',
-        accessor: 'name',
-      },
-      {
-        Header: 'Title',
-        accessor: 'title',
-      },
-    ],
-    []
-  );
-
   function useFetch(uri) {
     console.log('useFetch is starting');
 
@@ -68,68 +48,9 @@ const PackageCatalog = (props) => {
 
   const uri = 'http://localhost:8000/datapackages';
   const { loading, data, error } = useFetch(uri);
-  let tableInstance = useTable({ columns, data });
+
   if (loading === true) return <h1>loading ...</h1>;
   if (error === true) return <pre>Error: {data}</pre>;
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
-  return (
-    <div>
-      <table {...getTableProps()}>
-        <thead>
-          {
-            // Loop over the header rows
-            headerGroups.map((headerGroup) => (
-              // Apply the header row props
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  // Loop over the headers in each row
-                  headerGroup.headers.map((column) => (
-                    // Apply the header cell props
-                    <th {...column.getHeaderProps()}>
-                      {
-                        // Render the header
-                        column.render('Header')
-                      }
-                    </th>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </thead>
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {
-            // Loop over the table rows
-            rows.map((row) => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map((cell) => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
-                          {
-                            // Render the cell contents
-                            cell.render('Cell')
-                          }
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
-    </div>
-  );
+  return <DataPackage data={data} />;
 };
 export default PackageCatalog;
