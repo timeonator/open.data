dc152c8956b5df58995539194d392a9957a0b218
timeonator <timeonator@gmail.com>
2021-08-22 15:21:50 -0700

    prettier changes

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,54 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: "/src/index.js",
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            inject:'head',
-        })
-    ],
-    devServer: {
-        port: 8080,
-        onListening: function (server) {
-            const port = server.listeningApp.address().port;
-            console.log('Listening on port:', port);
+  mode: 'development',
+  entry: '/src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      inject: 'head',
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    onListening: function (server) {
+      const port = server.listeningApp.address().port;
+      console.log('Listening on port:', port);
+    },
+    historyApiFallback: true,
+    publicPath: '/',
+    contentBase: '/dist',
+  },
+  output: {
+    publicPath: '/',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules[/\\](?!react-data-grid[/\\]lib)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
-        historyApiFallback: true,
-        publicPath: "/",
-        contentBase: '/dist',
-    },
-    output: {
-        publicPath: "/",
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules : [
-            {
-                test: /\.js$/,
-                exclude: (/node_modules/),
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    }
-                }                
-            },
-            {
-                test: /\.css$/,
-                use: 
-                    [
-                        {loader: 'style-loader'},
-                        {loader: 'css-loader'},
-
-                    ]
-                
-            }
-
-        ]
-    }
-}
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+};
