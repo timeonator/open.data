73b96c41fb3ef2c5ff843a5b3f433debdb8f74f3
timeonator <timeonator@gmail.com>
2021-08-25 23:59:34 -0700

    added type info

---
+++ src/component/frictionless/licenses.tsx
@@ -0,0 +1,40 @@
+// "licenses": [{
+//     "name": "ODC-PDDL-1.0",
+//     "path": "http://opendatacommons.org/licenses/pddl/",
+//     "title": "Open Data Commons Public Domain Dedication and License v1.0"
+//   }]
+import React from 'react';
+import Table from '../Table';
+interface Heading {
+  accessor: string;
+  Header: string;
+}
+export interface License {
+  name: string;
+  path: string;
+  title: string;
+}
+
+const columns: Array<Heading> = [
+  { accessor: 'name', Header: 'Name' },
+  { accessor: 'path', Header: 'Path' },
+  { accessor: 'title', Header: 'Title' },
+];
+
+const data: Array<License> = [
+  {
+    name: 'ODC-PDDL-1.0',
+    path: 'http://opendatacommons.org/licenses/pddl/',
+    title: 'Open Data Commons Public Domain Dedication and License v1.0',
+  },
+];
+
+const Licenses = () => {
+  return (
+    <>
+      <h4>Resources</h4>
+      <Table columns={columns} data={data} />
+    </>
+  );
+};
+export default Licenses;
